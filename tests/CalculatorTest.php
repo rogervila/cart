6bb1c11d0c346a3c42404ff69517683c58929b43
Roger Vilà <rogervila@me.com>
2016-11-11 12:53:50 +0100

    calculate subtotal test

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Cart\Tests;
+
+use Cart\Cart;
+use Cart\Item;
+use Money\Money;
+
+class CalculatorTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function calculateCartSubtotalWithoutCurrency()
+    {
+        $cart = new Cart();
+
+        $items = [
+            new Item([
+                'id'    => uniqid(),
+                'price' => 1000
+            ]),
+            new Item([
+                'id'    => uniqid(),
+                'price' => '5,50'
+            ]),
+            new Item([
+                'id'    => uniqid(),
+                'price' => '4.50'
+            ])
+        ];
+        $expectedResult = 20.00;
+
+        $cart->add($items);
+
+        $this->assertEquals($expectedResult, $cart->subtotal());
+    }
+
+    /** @test */
+    public function calculateCartSubtotalWithCurrency()
+    {
+        $cart = new Cart();
+        $cart->currency('EUR');
+
+        $items = [
+            new Item([
+                'id'    => uniqid(),
+                'price' => 1000
+            ]),
+            new Item([
+                'id'    => uniqid(),
+                'price' => '5,50'
+            ]),
+            new Item([
+                'id'    => uniqid(),
+                'price' => '4.50'
+            ])
+        ];
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        $expectedResult = Money::EUR(2000);
+
+        $cart->add($items);
+
+        $this->assertEquals($expectedResult, $cart->subtotal());
+    }
+}
