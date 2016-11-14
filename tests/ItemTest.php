15bd6c167e8c81d6a1292bf31324686fdc8d84d5
Roger Vilà <rogervila@me.com>
2016-11-14 14:27:53 +0100

    test Item id

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -7,24 +7,35 @@ use Cart\Item;
 class ItemTest extends \PHPUnit_Framework_TestCase
 {
     /** @test */
-    public function createItem() {
+    public function createItem()
+    {
+
         $item = new Item('123');
 
         // Is an Item
         $this->assertInstanceOf('Cart\Item', $item);
 
         // Check Properties
-        foreach (['id'] as $property) {
+        foreach (['id', 'name', 'quantity', 'fields', 'price'] as $property) {
             $this->assertObjectHasAttribute($property, $item);
         }
 
         // Check methods
-        foreach (['id'] as $method) {
+        foreach (['id', 'name', 'quantity', 'fields', 'price'] as $method) {
             $this->assertTrue(
                 method_exists($item, $method),
                 'Cart Item does not have method "' . $method . '"'
             );
         }
+    }
+
+    /** @test */
+    public function itemReceivesAnIdAndReturnsIt()
+    {
+        $id   = uniqid();
+        $item = new Item($id);
+
+        $this->assertEquals($item->id(), $id);
     }
 
     /** @test */
